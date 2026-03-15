189fed31c0380f885a1cacf4b7bd311db8f9ce7c
Guilherme <guifrodrigues007@gmail.com>
2026-03-15 20:30:07 +0000

    Alteração - Coloquei apenas um espaço

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,8 +4,8 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main (){
-// Área para definição das variáveis para armazenar as propriedades das cidades
+int main () {
+  // Área para definição das variáveis para armazenar as propriedades das cidades
 
 char Estado1[20];
 char Estado2[20];
